0e571585cfe2049bc748ae18451a26f88dc2a311
Amigo <felipe.dioliveira@outlook.com>
2018-09-21 00:03:42 -0300

    Realizando login, autentificação via firebase

--- app/src/main/java/com/example/guilhermeserra/challlengesirio/SignUpActivity.java
+++ app/src/main/java/com/example/guilhermeserra/challlengesirio/SignUpActivity.java
@@ -7,17 +7,20 @@ import android.os.Bundle;
 import android.util.Patterns;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
 
 public class SignUpActivity extends AppCompatActivity implements View.OnClickListener{
 
     private FirebaseAuth mAuth;
 
+    ProgressBar progressBar;
     EditText edtEmail, edtPassword;
 
     @Override
@@ -27,8 +30,9 @@ public class SignUpActivity extends AppCompatActivity implements View.OnClickLis
 
         mAuth = FirebaseAuth.getInstance();
 
-        edtEmail = findViewById(R.id.edtEmail);
-        edtPassword = findViewById(R.id.edtPassword);
+        edtEmail = (EditText) findViewById(R.id.edtEmail);
+        edtPassword = (EditText) findViewById(R.id.edtPassword);
+        progressBar = (ProgressBar) findViewById(R.id.progressbar);
 
         findViewById(R.id.cardViewLogin).setOnClickListener(this);
         findViewById(R.id.txtViewSignIn).setOnClickListener(this);
@@ -37,9 +41,6 @@ public class SignUpActivity extends AppCompatActivity implements View.OnClickLis
     private void registerUser(){
         String email = edtEmail.getText().toString().trim();
         String password = edtPassword.getText().toString().trim();
-
-
-
 
 
         if(email.isEmpty()){
@@ -67,12 +68,24 @@ public class SignUpActivity extends AppCompatActivity implements View.OnClickLis
         }
 
 
+        progressBar.setVisibility(View.VISIBLE);
+
         mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
             @Override
             public void onComplete(@NonNull Task<AuthResult> task) {
+                progressBar.setVisibility(View.GONE);
                 if(task.isSuccessful()){
-
-                    Toast.makeText(getApplicationContext(), "User registered successfully", Toast.LENGTH_LONG).show();
+                    Toast.makeText(getApplicationContext(), "User registered successfully", Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(SignUpActivity.this, MainActivity.class);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                    startActivity(intent);
+                }else{
+                    if(task.getException() instanceof FirebaseAuthUserCollisionException){
+                        Toast.makeText(getApplicationContext(), "User already registered, try a different email", Toast.LENGTH_SHORT).show();
+                    }else{
+                        Toast.makeText(getApplicationContext(), task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                        progressBar.setVisibility(View.GONE);
+                    }
                 }
             }
         });
